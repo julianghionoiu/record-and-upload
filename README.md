ead9a9cdfd6a0d315eb0038b130c01ce8b7c0b60
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-05-17 22:23:15 +0100

    Improve the displayed metrics

--- README.md
+++ README.md
@@ -8,9 +8,14 @@ Command-line tool that records the screen as Fragmented MP4 and upload the fragm
 ### Build and run as command-line app
 
 This will grate a maven based Jar that will download the required dependencies before running the app:
-```
+```bash
 ./gradlew clean mavenCapsule
 rm -R ~/.capsule/deps/ro
 java -jar ./build/libs/record-and-upload-0.0.4-SNAPSHOT-capsule.jar --unique-id X \
             --config .private/aws-test-secrets --store ./build/play
+```
+
+To generate test files you could run
+```bash
+ mkdir -p build/play && dd if=/dev/random of=build/play/output_`date +%s`.mp4  bs=1m  count=16
 ```
--- src/main/java/tdl/record_upload/MetricsReportingTask.java
+++ src/main/java/tdl/record_upload/MetricsReportingTask.java
@@ -4,61 +4,120 @@ import lombok.extern.slf4j.Slf4j;
 import tdl.record.metrics.RecordingMetricsCollector;
 import tdl.s3.sync.progress.UploadStatsProgressListener;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.NumberFormat;
 import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
 import java.util.Timer;
 import java.util.TimerTask;
 
 @Slf4j
 class MetricsReportingTask {
     private static final NumberFormat percentageFormatter = NumberFormat.getPercentInstance();
+    private static final NumberFormat sizeFormatter = NumberFormat.getNumberInstance();
     private static final NumberFormat uploadSpeedFormatter = NumberFormat.getNumberInstance();
-    static {
-        percentageFormatter.setMinimumFractionDigits(1);
-        uploadSpeedFormatter.setMinimumFractionDigits(1);
-    }
+    private static final DateTimeFormatter durationFormatter = DateTimeFormatter.ofPattern("H'h'mm'm'ss's'");
 
+    static {
+        setFormatter(percentageFormatter, 1);
+        setFormatter(sizeFormatter, 2);
+        setFormatter(uploadSpeedFormatter, 3);
+    }
+    private static final double RENDERER_LOAD_THRESHOLD = 0.90;
+
+    private static void setFormatter(NumberFormat formatter, int digits) {
+        formatter.setMinimumFractionDigits(digits);
+        formatter.setMaximumFractionDigits(digits);
+    }
 
     private final Timer metricsTimer;
     private final UploadStatsProgressListener uploadStatsProgressListener;
     private final RecordingMetricsCollector recordingMetricsCollector;
     private final StringBuilder displayBuffer;
+    private final Formatter stringFormatter;
+    private int runCounter;
 
     MetricsReportingTask(RecordingMetricsCollector recordingMetricsCollector,
                          UploadStatsProgressListener uploadStatsProgressListener) {
+        this.runCounter = 0;
         this.recordingMetricsCollector = recordingMetricsCollector;
         this.uploadStatsProgressListener = uploadStatsProgressListener;
         this.metricsTimer = new Timer("MetricsTask");
         this.displayBuffer = new StringBuilder();
+        this.stringFormatter = new Formatter(displayBuffer);
     }
 
     void scheduleReportMetricsEvery(Duration delayBetweenRuns) {
         metricsTimer.schedule(new TimerTask() {
             @Override
             public void run() {
+                runCounter++;
+                checkForRecordingDrift();
                 displayMetrics();
             }
         }, 0, delayBetweenRuns.toMillis());
+    }
+
+    private void checkForRecordingDrift() {
+        if (onceEveryCoupleOfRuns() &&
+                recordingMetricsCollector.isCurrentlyRecording() &&
+                recordingMetricsCollector.getRenderingTimeRatio() > RENDERER_LOAD_THRESHOLD) {
+            log.warn(String.format("Renderer load is above %s, the recording will experience time drift",
+                    percentageFormatter.format(RENDERER_LOAD_THRESHOLD)));
+        }
     }
 
     private void displayMetrics() {
         displayBuffer.setLength(0);
 
         if (recordingMetricsCollector.isCurrentlyRecording()) {
-            displayBuffer.append("Recorded ").append(recordingMetricsCollector.getTotalFrames()).append(" frames");
-            displayBuffer.append(", load is ").append(percentageFormatter.format(recordingMetricsCollector.getRenderingTimeRatio()));
+            // Compute duration
+            int fps = recordingMetricsCollector.getInputFrameRate().getDenominator();
+            long recordedSeconds = recordingMetricsCollector.getTotalFrames() / fps;
+            LocalTime recodedTime = LocalTime.MIDNIGHT.plus(Duration.ofSeconds(recordedSeconds));
+
+            // Compute filesize
+            long fileSize = 0;
+            try {
+                fileSize = Files.size(recordingMetricsCollector.getDestinationPath());
+            } catch (IOException e) {
+                log.debug("Could not obtain filesize information");
+            }
+
+            stringFormatter.format("Recorded %8s, %5d frames, %6s MB",
+                    durationFormatter.format(recodedTime),
+                    recordingMetricsCollector.getTotalFrames(),
+                    sizeFormatter.format(bytes_to_mb(fileSize)));
+        }
+
+        if (recordingMetricsCollector.isCurrentlyRecording() && uploadStatsProgressListener.isCurrentlyUploading()) {
             displayBuffer.append(" | ");
         }
 
         if (uploadStatsProgressListener.isCurrentlyUploading()) {
-            uploadStatsProgressListener.getCurrentStats().ifPresent(fileUploadStat -> displayBuffer.append("Uploaded: ").append(percentageFormatter.format(fileUploadStat.getUploadRatio())).append(". ")
-                    .append(fileUploadStat.getUploadedSize()).append("/").append(fileUploadStat.getTotalSize()).append(" bytes. ")
-                    .append(uploadSpeedFormatter.format(fileUploadStat.getMBps())).append(" Mbps"));
+            uploadStatsProgressListener.getCurrentStats().ifPresent(fileUploadStat ->
+                    stringFormatter.format("Uploaded %5s of %6s MB at %5s MB/sec",
+                            percentageFormatter.format(fileUploadStat.getUploadRatio()),
+                            sizeFormatter.format(bytes_to_mb(fileUploadStat.getTotalSize())),
+                            uploadSpeedFormatter.format(fileUploadStat.getMBps())));
         }
 
         if(displayBuffer.length() > 0){
             log.info(displayBuffer.toString());
         }
+    }
+
+    //~~~~ Helpers
+
+    private boolean onceEveryCoupleOfRuns() {
+        return runCounter % 20 == 0;
+    }
+
+    private static double bytes_to_mb(double totalSize) {
+        return totalSize/((double)1024*1024);
     }
 
 
