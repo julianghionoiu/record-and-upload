9de8d8d449d86d858872398ca54cc97cc50013f9
neomatrix369 <sadhak001@gmail.com>
2018-12-05 17:33:37 +0000

    Adapted the windows package creator script to the way the Linux and MacOS scripts work. One minor point, due to the launcher not executing in windows we go the java -jar route, which changes nothing in principle

--- scripts/packr-scripts/windows/win-pack.sh
+++ scripts/packr-scripts/windows/win-pack.sh
@@ -14,21 +14,29 @@ if [[ -z ${JRE_ZIP_FILE_NAME} ]]; then
    exit -1
 fi
 
+# This would be a wget command that downloads the jar from the record-and-upload git repo
+# after the release process has push the artifact to github
+# the artifact will contain the OS specific humble video and the uber jar for record-and-upload
+RELEASE_VERSION=$(git describe --abbrev=0 --tags)
+RECORD_AND_UPLOAD_JAR=${PACKAGE_NAME}-${RELEASE_VERSION}.jar
+ZIP_ARCHIVE_NAME=$(getArchiveName "${RELEASE_VERSION}-windows" "zip")
+
 if [[ ! -s ${RECORD_AND_UPLOAD_JAR} ]]; then
    echo "Jar file ${RECORD_AND_UPLOAD_JAR} not found"
-   echo "Process halted."
-   exit -1
+   echo "Downloading ${RECORD_AND_UPLOAD_JAR} from github"
+   wget https://github.com/julianghionoiu/record-and-upload/releases/download/${RELEASE_VERSION}/${RECORD_AND_UPLOAD_JAR} ${PACKAGE_NAME}
 fi
 
 echo 
 echo "*** Removing the old image folder: ${PACKR_TARGET_FOLDER} ***"
 rm -fr ${PACKR_TARGET_FOLDER} || true
 
-ZIP_ARCHIVE_NAME=$(getZipArchiveName "windows")
-echo 
+echo
 echo "*** Removing the old ${ZIP_ARCHIVE_NAME} archive ***"
 rm -fr ${ZIP_ARCHIVE_NAME} || true
 
+echo
+echo "*** Building '${PACKAGE_NAME}' image using packr ***"
 time java -jar ../packr.jar \
      --platform windows64 \
      --executable ${EXE_NAME} \
@@ -36,7 +44,15 @@ time java -jar ../packr.jar \
      --jdk ${JRE_ZIP_FILE_NAME} \
      --mainclass tdl.record_upload.RecordAndUploadApp \
      --vmargs Xmx2G \
-     --minimizejre ../reduced-jre.json \
      --output ${PACKR_TARGET_FOLDER}
 
-time cd ${PACKR_TARGET_FOLDER} && zip -r ../${ZIP_ARCHIVE_NAME} .
+HUMBLE_WINDOWS_LIB=libhumblevideo-0.dll
+echo "*** Uncompressing  ${HUMBLE_WINDOWS_LIB} from ${RECORD_AND_UPLOAD_JAR} into '${PACKR_TARGET_FOLDER}' ***"
+time unzip -o ${PACKR_TARGET_FOLDER}/${RECORD_AND_UPLOAD_JAR} ${HUMBLE_WINDOWS_LIB}
+mv ${HUMBLE_WINDOWS_LIB} ${PACKR_TARGET_FOLDER}
+
+echo "*** Removing ${HUMBLE_WINDOWS_LIB} from ${RECORD_AND_UPLOAD_JAR} in '${PACKR_TARGET_FOLDER}' ***"
+time zip -d ${PACKR_TARGET_FOLDER}/${RECORD_AND_UPLOAD_JAR} ${HUMBLE_WINDOWS_LIB}
+
+echo "*** Compressing '${PACKR_TARGET_FOLDER}' into '${ZIP_ARCHIVE_NAME}' ***"
+time zip -r ${ZIP_ARCHIVE_NAME} ${PACKR_TARGET_FOLDER}
