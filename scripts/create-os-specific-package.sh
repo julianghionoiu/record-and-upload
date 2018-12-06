7d2484afeb169b1196e87faefb97ae204b6e24e1
neomatrix369 <sadhak001@gmail.com>
2018-12-06 22:13:28 +0000

    Refactored create-os-specific-package.sh to be able to run from both inside and outside the scripts folder and perform other checks to be able build OS-specific package

--- scripts/create-os-specific-package.sh
+++ scripts/create-os-specific-package.sh
@@ -5,14 +5,36 @@ set -u
 set -o pipefail
 
 OS_NAME=${1:-linux}
-RELEASE_ARCHIVE_EXT=${2:-zip}
+RELEASE_ARCHIVE_EXT=${2:-tgz}
 
 RELEASE_VERSION=`cat version.txt`
+SCRIPT_DIR=$(dirname $0)
+PARENT_DIR=$(realpath ${SCRIPT_DIR}/..)
+PATH_TO_JAR=${PARENT_DIR}/build/libs
+JAR_FILE_NAME=$(ls ${PATH_TO_JAR}/record-and-upload-${OS_NAME}-${RELEASE_VERSION}*.jar || true)
+
+if [[ -z "${JAR_FILE_NAME}" ]]; then
+   echo "Cannot find the ${OS_NAME} specific jar file in ${PATH_TO_JAR} folder, please check if it has been built"
+   exit -1
+fi
+
+NEW_JAR_FILE_NAME="record-and-upload-${OS_NAME}-${RELEASE_VERSION}.jar"
 PACKAGE_NAME="record-and-upload-${OS_NAME}-${RELEASE_VERSION}.${RELEASE_ARCHIVE_EXT}"
 
-echo "*** Building a package with ${PACKAGE_NAME} for ${OS_NAME} ***"
-source ./scripts/packr-scripts/common-env-variables.sh
-cp ${PACKAGE_NAME} scripts/packr-scripts/${OS_NAME}
-./scripts/packr-scripts/${OS_NAME}/${OS_NAME}-pack.sh
-echo "*** Finished building a package with ${PACKAGE_NAME} for ${OS_NAME} ***"
+echo "*** Building package ${PACKAGE_NAME} ***"
+source ${SCRIPT_DIR}/packr-scripts/common-env-variables.sh
 
+echo "Removing old ${PACKAGE_NAME} file(s)"
+rm ${SCRIPT_DIR}/packr-scripts/${OS_NAME}/*.jar
+
+echo "Copying ${PACKAGE_NAME} into the ${SCRIPT_DIR}/packr-scripts/${OS_NAME}/ folder"
+cp ${JAR_FILE_NAME} ${SCRIPT_DIR}/packr-scripts/${OS_NAME}/${NEW_JAR_FILE_NAME}
+
+${SCRIPT_DIR}/packr-scripts/${OS_NAME}/${OS_NAME}-pack.sh
+
+if [[ -s ${SCRIPT_DIR}/packr-scripts/${OS_NAME}/${PACKAGE_NAME} ]]; then
+  echo "*** Finished building package ${PACKAGE_NAME} ***"
+else
+  echo "*** Failed to build ${PACKAGE_NAME} ***"
+  exit -1
+fi
